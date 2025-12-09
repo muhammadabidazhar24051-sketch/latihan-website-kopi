39805e1c034194537f164e9358c82ed5fb820b94
Abidazhar <muhammad.abid.azhar24051@mhs.uingusdur.ac.id>
2025-12-09 15:31:13 +0700

    inisialisasi

---
+++ script.js
@@ -0,0 +1,16 @@
+// memberi tahu bahwa sccript berhasil diload
+alert('Selamat Datang di Website Profil UMKM Kopi Nusantara')
+
+// Menampilkan informasi variable di console
+const namaUMKM = 'Kopi Nusantara'
+console.log('Setelah penambahan:', produk);
+
+// Demonstrasi variable dan operator
+let produk = 3;
+console.log('jumlah produk saat ini:', produk);
+produk+= 2;
+console.log('Setelah penambahan:', produk);
+
+
+
+
